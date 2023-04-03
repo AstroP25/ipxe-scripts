07e04232642ed7ab39db8d45651aa095dfef077a
AstroP25 <82893489+AstroP25@users.noreply.github.com>
2023-04-03 14:49:01 -0500

    added windows scripts

---
+++ ltsc.bat
@@ -0,0 +1,3 @@
+wpeinit
+net use \\172.31.2.2\win_install /user:workgroup\zapman Teloca92!
+\\172.31.2.2\win_install\en-us-windows_10_ltsc+2021\setup.exe
---
+++ windows7.bat
@@ -0,0 +1,3 @@
+wpeinit
+net use \\172.31.2.2\win_install /user:workgroup\zapman Teloca92!
+\\172.31.2.2\win_install\en-us-windows_7_ultimate_x86\setup.exe
